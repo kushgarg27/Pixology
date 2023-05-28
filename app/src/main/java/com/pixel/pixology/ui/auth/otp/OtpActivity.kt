2178e1cfb3fb17577e26214fc1ab78c443dac1ed
kushgarg <kushgarg277@gmail.com>
2023-05-28 11:26:08 +0530

    OTP verfication Done

--- app/src/main/java/com/pixel/pixology/ui/auth/otp/OtpActivity.kt
+++ app/src/main/java/com/pixel/pixology/ui/auth/otp/OtpActivity.kt
@@ -2,13 +2,12 @@
 
 package com.pixel.pixology.ui.auth.otp
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.goodiebag.pinview.Pinview
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthProvider
 import com.pixel.pixology.R
@@ -16,67 +15,49 @@ import com.pixel.pixology.databinding.ActivityOtpBinding
 import com.pixel.pixology.ui.home.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
 
-
 @AndroidEntryPoint
 class OtpActivity : AppCompatActivity() {
 
-    private var VerficationID: String? = null
-
-
+    private var verificationId: String? = null
     private lateinit var binding: ActivityOtpBinding
-    @SuppressLint("SetTextI18n")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityOtpBinding.inflate(layoutInflater, null, false)
+        binding = ActivityOtpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val window = this.window
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-        window.statusBarColor = this.resources.getColor(R.color.theme)
+        window.statusBarColor = resources.getColor(R.color.theme)
 
-        binding.phoneNUm.text = "+91" + intent.getStringExtra("phone")
-        VerficationID = intent.getStringExtra("verficationId")
+        verificationId = intent.getStringExtra("verificationId")
+
         binding.tvResend.setOnClickListener {
-            Toast.makeText(
-                this@OtpActivity,
-                "otp resend successfully ",
-                Toast.LENGTH_SHORT
-            ).show()
+            Toast.makeText(this@OtpActivity, "OTP Resent", Toast.LENGTH_SHORT).show()
         }
-        binding.btnVerify.setOnClickListener {
-            binding.progressbar.visibility = View.VISIBLE
-            binding.btnVerify.visibility = View.INVISIBLE
-            if (binding.etOtp.text.toString().trim { it <= ' ' }.isEmpty()) {
-                binding.etOtp.error = "otp cant be empty"
-                binding.etOtp.requestFocus()
-            } else {
-                if (VerficationID != null) {
-                    val code: String = binding.etOtp.text.toString().trim { it <= ' ' }
-                    val credential = PhoneAuthProvider.getCredential(VerficationID!!, code)
-                    FirebaseAuth.getInstance().signInWithCredential(credential)
-                        .addOnCompleteListener { task ->
-                            if (task.isSuccessful) {
-                                val intent = Intent(this@OtpActivity, HomeActivity::class.java)
-                                intent.flags =
-                                    Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                                binding.progressbar.visibility = View.VISIBLE
-                                binding.btnVerify.visibility = View.INVISIBLE
-                                startActivity(intent)
-                            } else {
-                                binding.progressbar.visibility = View.GONE
-                                binding.btnVerify.visibility = View.VISIBLE
-                                Toast.makeText(
-                                    this@OtpActivity,
-                                    "otp not valid",
-                                    Toast.LENGTH_SHORT
-                                )
-                                    .show()
-                            }
-                        }
+
+        binding.etOtp.setPinViewEventListener(object : Pinview.PinViewEventListener {
+            override fun onDataEntered(pinview: Pinview?, fromUser: Boolean) {
+                val otp = pinview?.value
+                if (!otp.isNullOrEmpty() && otp.length == 6) {
+                    verifyOtp(otp)
                 }
             }
-        }
+        })
+    }
 
+    private fun verifyOtp(otp: String) {
+        val credential = PhoneAuthProvider.getCredential(verificationId!!, otp)
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@OtpActivity, HomeActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@OtpActivity, "Invalid OTP", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
