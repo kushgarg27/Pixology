d2683f5a01801c830485051bf132b1b03649e18a
kushgarg <kushgarg277@gmail.com>
2023-05-31 21:34:05 +0530

    QR Code Scann

--- app/src/main/java/com/pixel/pixology/ui/scanandsearch/GoogleLensActivity.kt
+++ app/src/main/java/com/pixel/pixology/ui/scanandsearch/GoogleLensActivity.kt
@@ -1,29 +1,30 @@
-
 package com.pixel.pixology.ui.scanandsearch
+
 import android.Manifest
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.SurfaceTexture
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
-import android.util.Size
-import android.view.Surface
-import android.view.TextureView
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 import com.pixel.pixology.R
-import java.io.ByteArrayOutputStream
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -32,7 +33,7 @@ import java.util.concurrent.Executors
 
 class GoogleLensActivity : AppCompatActivity() {
     private lateinit var cameraExecutor: ExecutorService
-    private lateinit var previewView: TextureView
+    private lateinit var previewView: PreviewView
     private lateinit var captureButton: Button
     private lateinit var capturedImage: ImageView
     private lateinit var resultText: TextView
@@ -46,7 +47,7 @@ class GoogleLensActivity : AppCompatActivity() {
         setContentView(R.layout.activity_google_lens)
 
         cameraExecutor = Executors.newSingleThreadExecutor()
-        previewView = findViewById(R.id.preview_view)
+        previewView = findViewById<PreviewView>(R.id.preview_view)
         captureButton = findViewById(R.id.capture_button)
         capturedImage = findViewById(R.id.captured_image)
         resultText = findViewById(R.id.result_text)
@@ -81,50 +82,36 @@ class GoogleLensActivity : AppCompatActivity() {
     }
 
     private fun bindPreview(cameraProvider: ProcessCameraProvider) {
-        val preview = Preview.Builder().build()
-
-        val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
-            override fun onSurfaceTextureAvailable(surfaceTexture: SurfaceTexture, width: Int, height: Int) {
-                val surface = Surface(surfaceTexture)
-                val previewSize = Size(width, height)
-                val rotation = previewView.display.rotation
-                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-                try {
-                    cameraProvider.unbindAll()
-//                    cameraProvider.bindToLifecycle(this@GoogleLensActivity, cameraSelector, preview.setSurfaceProvider(surface), imageCapture)
-                } catch (e: Exception) {
-                    Log.e("CameraX", "Error: ${e.message}")
-                }
-            }
-
-            override fun onSurfaceTextureSizeChanged(surfaceTexture: SurfaceTexture, width: Int, height: Int) {}
-
-            override fun onSurfaceTextureDestroyed(surfaceTexture: SurfaceTexture): Boolean {
-                return true
-            }
-
-            override fun onSurfaceTextureUpdated(surfaceTexture: SurfaceTexture) {}
+        val preview = Preview.Builder().build().also {
+            it.setSurfaceProvider(previewView.surfaceProvider)
         }
 
-        previewView.surfaceTextureListener = surfaceTextureListener
+        imageCapture = ImageCapture.Builder()
+            .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+            .build()
+
+        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+        try {
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview, imageCapture)
+        } catch (e: Exception) {
+            Log.e("CameraX", "Error: ${e.message}")
+        }
     }
-
-
-
 
     private fun captureImage() {
         val imageCapture = imageCapture ?: return
 
-        val file = createImageFile()
-
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(file).build()
+        val photoFile = createImageFile()
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
 
         imageCapture.takePicture(
             outputOptions, cameraExecutor, object : ImageCapture.OnImageSavedCallback {
+                @RequiresApi(Build.VERSION_CODES.P)
                 override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    val savedUri = outputFileResults.savedUri
-                    val bitmap = savedUri?.let { getBitmapFromUri(it) }
+                    val savedUri = outputFileResults.savedUri ?: Uri.fromFile(photoFile)
+                    val bitmap = getBitmapFromUri(savedUri)
                     bitmap?.let { showCapturedImage(it) }
                 }
 
@@ -136,18 +123,16 @@ class GoogleLensActivity : AppCompatActivity() {
     }
 
     private fun createImageFile(): File {
-        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
-        val storageDir = filesDir
-        return File.createTempFile("IMG_$timestamp", ".jpg", storageDir)
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("IMG_$timeStamp", ".jpg", storageDir)
     }
 
-    private fun getBitmapFromUri(uri: android.net.Uri): Bitmap? {
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun getBitmapFromUri(uri: Uri): Bitmap? {
         return try {
-            val inputStream = contentResolver.openInputStream(uri)
-            val byteArray = inputStream?.readBytes()
-            byteArray?.let {
-                BitmapFactory.decodeByteArray(it, 0, it.size)
-            }
+            val source = ImageDecoder.createSource(contentResolver, uri)
+            ImageDecoder.decodeBitmap(source)
         } catch (e: Exception) {
             Log.e("CameraX", "Error loading bitmap: ${e.message}", e)
             null
@@ -155,8 +140,10 @@ class GoogleLensActivity : AppCompatActivity() {
     }
 
     private fun showCapturedImage(bitmap: Bitmap) {
-        capturedImage.setImageBitmap(bitmap)
-        capturedImage.visibility = View.VISIBLE
+        runOnUiThread {
+            capturedImage.setImageBitmap(bitmap)
+            capturedImage.visibility = View.VISIBLE
+        }
     }
 
     override fun onDestroy() {
